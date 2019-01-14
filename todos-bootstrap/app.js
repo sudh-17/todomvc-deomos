38c80467eddbe7d6f1f9fa44324734ea6650b621
sudh-17 <395338095@qq.com>
2019-01-14 09:17:10 +0800

    update app.js

--- todos-bootstrap/app.js
+++ todos-bootstrap/app.js
@@ -1,7 +1,7 @@
 (function(){
 
     function Todo(){
-        this.store = new app.Store('todos');
+        this.store = new app.Store('todos1');
 		this.model = new app.Model(this.store);
 		this.view = new app.View();
 		this.controller = new app.Controller(this.model, this.view);
